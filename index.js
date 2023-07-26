d0b44e8abffa7fe76b559f8d55467519e49515be
Nahimana <mnahim024@gmail.com>
2023-07-26 05:56:17 +0200

    final version

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const hiddenSection = document.querySelector(".hidden-section");
 const productsLink = document.querySelector(".products");
+const signIn = document.querySelector(".sign-in");
 productsLink.addEventListener("click", () => {
   hiddenSection.classList.toggle("flex");
+  // signIn.style.zIndex = "0";
 });
 
 const navBar = document.querySelector("header nav");
-const signIn = document.querySelector(".sign-in");
+const xIcone = document.querySelector(".x-icone");
 const menu = document.querySelector(".menu");
 menu.addEventListener("click", () => {
   navBar.classList.toggle("translate");
-  signIn.classList.toggle("translate");
+  hiddenSection.style.display = "none";
+});
+
+xIcone.addEventListener("click", () => {
+  hiddenSection.style.display = "none";
 });
